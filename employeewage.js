df2ad4650a6efa6dc508943c1a7aeeb911ed8017
surendra-chouhan <chouhansurendra028@gmail.com>
2021-04-11 20:48:06 +0530

    Added log to calculate wages for a month

--- employeewage.js
+++ employeewage.js
@@ -3,6 +3,7 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
+const MAX_NUMBER_OF_WORKING_DAYS = 20;
 
 function getempHrs(empCheck){
     switch (empCheck) {
@@ -16,7 +17,10 @@ function getempHrs(empCheck){
 }
 
 let empHrs = 0;
-let empCheck = Math.floor(Math.random() * 10) % 3;
-empHrs = getempHrs(empCheck);
+for(let i = 0; i <= MAX_NUMBER_OF_WORKING_DAYS; i++) {
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    empHrs += getempHrs(empCheck);
+}
 let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Daily Wage of an employee is : " + empWage);
+console.log("Total Hours : " + empHrs);
+console.log("Monthly Wage of an employee is : " + empWage);
